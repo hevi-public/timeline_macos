d03348132aadcadb65649d98e45b9f2e181c6d3c
Hevi <hevi.public@gmail.com>
2019-03-08 21:30:08 +0000

    Initial Commit

---
+++ Timeline/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timeline
+//
+//  Created by Hevi on 08/03/2019.
+//  Copyright © 2019 Hevi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
