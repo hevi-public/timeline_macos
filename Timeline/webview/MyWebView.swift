bdb5deadd68c684c846716cbd0cfd8300a110678
Hevi <hevi.public@gmail.com>
2019-03-09 21:05:19 +0000

    data from Swift

--- Timeline/webview/MyWebView.swift
+++ Timeline/webview/MyWebView.swift
@@ -11,8 +11,6 @@ import WebKit
 
 class MyWebView: WKWebView, WKScriptMessageHandler {
     
-    
-
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
@@ -34,7 +32,17 @@ class MyWebView: WKWebView, WKScriptMessageHandler {
     func userContentController(_ userContentConvarller: WKUserContentController, didReceive message: WKScriptMessage) {
         switch message.name {
         case "webviewreadyHandler":
-            self.initGraph()
+            
+            var timelineData = [TimelineItem]()
+            timelineData.append(ExampleDataItem(id: 1, content: "item 1", start: "2019-02-20", end: "2019-02-25"))
+            timelineData.append(ExampleDataItem(id: 2, content: "item 2", start: "2019-02-14", end: "2019-02-18"))
+            timelineData.append(ExampleDataItem(id: 3, content: "item 3", start: "2019-02-15", end: "2019-02-23"))
+            timelineData.append(ExampleDataItem(id: 4, content: "item 4", start: "2019-02-22", end: "2019-02-28"))
+            timelineData.append(ExampleDataItem(id: 5, content: "item 5", start: "2019-02-12", end: "2019-02-14"))
+            timelineData.append(ExampleDataItem(id: 6, content: "item 6", start: "2019-02-17", end: "2019-02-20"))
+            
+          
+            self.initGraph(timelineItems: timelineData)
             break
         case "linkNodesHandler":
             let body = message.body as! [String : String]
@@ -44,12 +52,17 @@ class MyWebView: WKWebView, WKScriptMessageHandler {
         }
     }
     
-    public func initGraph() {
+    public func initGraph(timelineItems: [TimelineItem]) {
+        
+        let items = timelineItems.map { timelineItem -> [String:Any] in
+            return timelineItem.asDict()
+        }
+        
         do {
-//            let jsonGraph = try JSONSerialization.data(withJSONObject: graph, options: .prettyPrinted)
-//            let jsonGraphString = String(data: jsonGraph, encoding: .utf8) ?? ""
-//
-            let script = NSString(format: "init();")
+            let jsonGraph = try JSONSerialization.data(withJSONObject: items, options: .prettyPrinted)
+            let jsonGraphString = String(data: jsonGraph, encoding: .utf8) ?? ""
+
+            let script = NSString(format: "init(%@);", jsonGraphString)
             self.evaluateJavaScript(script as String, completionHandler: { (result, error) in
                 if error != nil {
                     print("error: " + (error! as NSError).debugDescription)
@@ -58,5 +71,31 @@ class MyWebView: WKWebView, WKScriptMessageHandler {
         } catch {
             print("Problem")
         }
+    }
+}
+
+protocol TimelineItem {
+    var id: Int { get set }
+    var content: String { get set }
+    var start: String { get set }
+    var end: String { get set }
+    
+    func asDict() -> [String : Any]
+}
+
+struct ExampleDataItem: TimelineItem {
+    
+    typealias StringToAnyDict = [String : Any]
+    
+    var id: Int
+    var content: String
+    var start: String
+    var end: String
+    
+    public func asDict() -> [String : Any] {
+        return ["id": self.id,
+                "content": self.content,
+                "start": self.start,
+                "end": self.end] as [String : Any]
     }
 }
